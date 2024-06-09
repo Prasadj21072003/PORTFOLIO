1ec7864a5b590547fa35242adbf3814a23562bf6
Prasadj21072003 <prasadjadhav2003@gmail.com>
2024-06-09 16:24:31 +0530

    twelve  commit

--- data.js
+++ data.js
@@ -1,40 +1,40 @@
 export const Projects = [
   {
     img: "/Screenshot(55).png",
-    title: "Non-responsive Netflix clone Website (MERN+JWT)",
+    title: "Non-responsive Netflix clone Website ",
 
     href: "https://netflix-clone-without-adminpanel-frontend.vercel.app/",
   },
   {
     img: "/Screenshot(8).png",
-    title: "Responsive Shoes Website (NEXTJS+TAILWIND)",
+    title: "Responsive Shoes Website ",
     para: " Tech used is  React and Tailwind css ",
     href: "https://effulgent-bonbon-07f4b5.netlify.app",
   },
   {
     img: "/Screenshot(53).png",
-    title: "Non-Responsive E-commerce website(REACTJS)",
+    title: "Non-Responsive E-commerce website",
     para: " Tech used is Reactjs and Scss ",
     href: "https://darling-shortbread-ce9734.netlify.app/",
   },
   {
     img: "/Screenshot(51).png",
-    title: "Non-Responsive Car website (REACTJS)",
+    title: "Non-Responsive Car website ",
     href: "https://lucky-cuchufli-2dea13.netlify.app",
   },
   {
     img: "/Screenshot(52).png",
-    title: "Non-Responsive Property website (REACTJS)",
+    title: "Non-Responsive Property website ",
     href: "https://strong-haupia-0bd045.netlify.app",
   },
   {
     img: "/Screenshot(54).png",
-    title: "Non-Responsive Adminpanel (REACTJS)",
+    title: "Non-Responsive Adminpanel ",
     href: "https://thriving-gumdrop-1b36d7.netlify.app",
   },
   {
     img: "/Screenshot(50).png",
-    title: "Responsive Website Design (REACTJS)",
+    title: "Responsive Website Design ",
     para: " Tech used is Reactjs and Tailwind css",
     href: "https://classy-chebakia-f84e67.netlify.app",
   },
