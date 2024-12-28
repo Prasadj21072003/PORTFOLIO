af60cff80950b995b0b04d7f07d447543ff6ba7a
PRASAD JADHAV <116389075+Prasadj21072003@users.noreply.github.com>
2024-12-28 16:00:01 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,49 +1,49 @@
 export const Projects = [
   {
-    img: "/Screenshot(62).png",
+    img: "./Screenshot(62).png",
     title: "Responsive Apple landing page ",
     href: "https://apple-phone-landing-page-website.onrender.com/",
   },
   {
-    img: "/Screenshot(56).png",
+    img: "./Screenshot(56).png",
     title: "Responsive Realtime chat website ",
     href: "https://mern-stack-chat-app-frontend.vercel.app/login",
   },
   {
-    img: "/Screenshot(55).png",
+    img: "./Screenshot(55).png",
     title: "Non-responsive Netflix clone Website ",
 
     href: "https://netflix-clone-without-adminpanel-frontend.vercel.app/",
   },
   {
-    img: "/Screenshot(8).png",
+    img: "./Screenshot(8).png",
     title: "Responsive Shoes Website ",
     para: " Tech used is  React and Tailwind css ",
     href: "https://effulgent-bonbon-07f4b5.netlify.app",
   },
   {
-    img: "/Screenshot(53).png",
+    img: "./Screenshot(53).png",
     title: "Non-Responsive E-commerce website",
     para: " Tech used is Reactjs and Scss ",
     href: "https://snazzy-flan-1ceb7a.netlify.app/",
   },
   {
-    img: "/Screenshot(51).png",
+    img: "./Screenshot(51).png",
     title: "Non-Responsive Car website ",
     href: "https://lucky-cuchufli-2dea13.netlify.app",
   },
   {
-    img: "/Screenshot(52).png",
+    img: "./Screenshot(52).png",
     title: "Non-Responsive Property website ",
     href: "https://strong-haupia-0bd045.netlify.app",
   },
   {
-    img: "/Screenshot(54).png",
+    img: "./Screenshot(54).png",
     title: "Non-Responsive Adminpanel ",
     href: "https://thriving-gumdrop-1b36d7.netlify.app",
   },
   {
-    img: "/Screenshot(50).png",
+    img: "./Screenshot(50).png",
     title: "Responsive Website Design ",
     para: " Tech used is Reactjs and Tailwind css",
     href: "https://classy-chebakia-f84e67.netlify.app",
